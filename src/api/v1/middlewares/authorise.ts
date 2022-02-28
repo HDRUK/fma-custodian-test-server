a033bb4548d91937f3955ce6d0bb02cb5ced3198
Callum Reekie <cdjreekie@gmail.com>
2022-02-28 09:41:18 +0000

    reconfig auth middleware

--- src/api/v1/middlewares/authorise.ts
+++ src/api/v1/middlewares/authorise.ts
@@ -8,13 +8,17 @@ import { CredentialsModel } from '../models/credentials.model';
 const authorise = (clientType: string) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         let auth: any = req.headers['authorization'];
-
         let token, apiKey;
-        if (auth && auth.split(' ')[0] === 'Bearer') {
-            token = auth.split(' ')[1];
-        } else if (auth && auth.split(' ')[0] === 'Basic'! && auth.split(' ')[1].split(':').length === 2) {
-            apiKey = auth.split(' ')[1];
-        } else {
+
+        try {
+            if (auth && auth.split(' ')[0] === 'Bearer') {
+                token = auth.split(' ')[1];
+            } else if (auth && auth.split(' ')[0] === 'Basic'! && auth.split(' ')[1].split(':').length === 2) {
+                apiKey = auth.split(' ')[1];
+            } else {
+                throw new Error();
+            }
+        } catch (err) {
             return res.status(401).send({
                 status: 'Unauthorised',
                 message: 'The authorization header value must be of the format "Basic <client_id>:<api_key>" or "Bearer <access_token>"',
