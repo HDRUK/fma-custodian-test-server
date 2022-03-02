bef1f8228a8a002deb4eb34a843ebc9266fbc4e8
Callum Reekie <cdjreekie@gmail.com>
2022-03-02 16:09:19 +0000

    fix types

--- src/api/v1/models/dataset.model.ts
+++ src/api/v1/models/dataset.model.ts
@@ -1,3 +1,4 @@
+import { Model } from 'mongoose';
 import db from '../config/database';
 
-export const Datasets = db.model('datasets', new db.Schema({}, { strict: false }), 'datasets');
+export const Datasets = <Model<any>>db.model('datasets', new db.Schema({}, { strict: false }), 'datasets');
