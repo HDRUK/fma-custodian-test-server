50a5e7202a6df7ae47805365928a809fb558f2d3
Callum Reekie <cdjreekie@gmail.com>
2022-03-14 12:08:52 +0000

    remove nested ifs

--- src/api/v1/middlewares/oauthValidation.ts
+++ src/api/v1/middlewares/oauthValidation.ts
@@ -3,24 +3,24 @@ import _ from 'lodash';
 import { Request, Response, NextFunction } from 'express';
 
 const oauthValidation = (req: Request, res: Response, next: NextFunction) => {
-    const { grant_type = '' }: { grant_type: string } = req.body;
+    const { client_id = '', client_secret = '', grant_type = '' }: { client_id: string; client_secret: string, grant_type: string } = req.body;
 
-    if (grant_type === 'client_credentials') {
-        const { client_id = '', client_secret = '' }: { client_id: string; client_secret: string } = req.body;
-
-        if (_.isEmpty(client_id) || _.isEmpty(client_secret)) {
-            return res.status(400).send({
-                status: 'error',
-                message: 'OAuth2Error: incomplete credentials',
-            });
-        }
-
-        next();
-    } else {
+    if (_.isEmpty(client_id) || _.isEmpty(client_secret) || _.isEmpty(grant_type)) {
         return res.status(400).send({
             status: 'error',
-            message: 'OAuth2Error: only the client_credentials grant type is supported',
+            message: 'OAuth2Error: incomplete credentials',
         });
+    }
+
+    switch (grant_type) {
+        case 'client_credentials':
+            next()
+            break;
+        default:
+            return res.status(400).send({
+                status: 'error',
+                message: 'OAuth2Error: only the client_credentials grant type is supported',
+            });
     }
 };
 
